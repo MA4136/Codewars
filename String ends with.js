59e66e06cb57712d490a28b7989b0e5d63636ac6
Molotov <bigdickmasterblaster@gmail.com>
2020-11-14 21:57:58 +0300

    first shot

---
+++ String ends with.js
@@ -0,0 +1,17 @@
+//      Training
+//      www.codewars.com
+
+/*  Instructions
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+    Examples
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+*/
+
+
+function solution(str, ending) {
+    return str.endsWith(ending)
+}
+
+// You have passed all of the tests! :)
