7f733bbcee4827610ac32536da0194a3396283ec
Molotov <bigdickmasterblaster@gmail.com>
2020-11-23 23:35:40 +0300

    first shot

---
+++ Shortest Word.js
@@ -0,0 +1,16 @@
+//      Shortest Word
+//      www.codewars.com
+
+/*  Instructions
+Simple, given a string of words, return the length of the shortest word(s).
+String will never be empty and you do not need to account for different data types.
+
+    Examples
+"bitcoin take over the world maybe who knows perhaps" ==> 3
+*/
+
+function findShort(str){
+    return Math.min(...str.split(" ").map(item=>item.length))
+}
+
+// You have passed all of the tests! :)
