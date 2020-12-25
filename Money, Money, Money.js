9562009df7b96c5bb6ba602a47d440a8e61c957b
Molotov <bigdickmasterblaster@gmail.com>
2020-12-25 21:41:21 +0300

    first shot

--- Money, Money, Money.js
+++ Money, Money, Money.js
@@ -15,11 +15,24 @@ calculateYears(1000,0.05,0.18,1000) ==> 0
 */
 
 function calculateYears(principal, interest, tax, desired) {
-    console.log(principal, interest, tax, desired)
+    if (principal === desired) {
+        return 0;
+    }
+    let years = 0;
+    while (principal < desired) {
+        let Interest = principal * interest;
+        let Tax = Interest * tax;
+        principal += (Interest - Tax);
+        years++;
+    }
+    return years;
 }
 
-console.log(calculateYears(1000, 0.05, 0.18, 1100))
-console.log(calculateYears(1000, 0.01625, 0.18, 1200))
-console.log(calculateYears(1000, 0.05, 0.18, 1000))
+// console.log(calculateYears(1000, 0.05, 0.18, 1100))
+// console.log(calculateYears(1000, 0.01625, 0.18, 1200))
+// console.log(calculateYears(1000, 0.05, 0.18, 1000))
 
 // node "Money, Money, Money"
+
+// Good Job!
+// You have passed all of the tests! :)
