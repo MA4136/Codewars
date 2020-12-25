7aa3edaadd43fe2f57a6a5191c93a040e00ead6c
Molotov <bigdickmasterblaster@gmail.com>
2020-12-25 21:34:16 +0300

    first shot

--- Growth of a Population.js
+++ Growth of a Population.js
@@ -21,9 +21,9 @@ function nbYear(p0, percent, aug, p) {
     return counter
 }
 
-console.log(nbYear(1500, 5, 100, 5000))
-console.log(nbYear(1500000, 2.5, 10000, 2000000))
-console.log(nbYear(1500000, 0.25, 1000, 2000000))
+// console.log(nbYear(1500, 5, 100, 5000))
+// console.log(nbYear(1500000, 2.5, 10000, 2000000))
+// console.log(nbYear(1500000, 0.25, 1000, 2000000))
 
 // node "Growth of a Population"
 
