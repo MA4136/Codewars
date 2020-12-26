808aafd7f3b2df604549dc1b5301b0de2b8b2efc
Molotov <bigdickmasterblaster@gmail.com>
2020-12-26 22:07:54 +0300

    first shot (not finished)

---
+++ Number of People in the Bus.js
@@ -0,0 +1,32 @@
+//      Number of People in the Bus
+//      www.codewars.com
+
+/*  Instructions
+There is a bus moving in the city, and it takes and drop some people in each bus stop.
+You are provided with a list (or array) of integer arrays (or tuples). Each integer array has two items which represent
+number of people get into bus (The first item) and number of people get off the bus (The second item) in a bus stop.
+Your task is to return number of people who are still in the bus after the last bus station (after the last array).
+Even though it is the last bus stop, the bus is not empty and some people are still in the bus, and they are probably sleeping there :D
+Take a look on the test cases.
+Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the return integer can't be negative.
+The second value in the first integer array is 0, since the bus is empty in the first bus stop.
+
+    Examples
+number([[10,0],[3,5],[5,8]]) ==> 5
+number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]]) ==> 17
+number([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]]) ==> 21
+*/
+
+function busStops(array) {
+    return +(array.map(el => el[0]).reduce((a, b) => a + b, 0) -
+        array.map(el => el[1]).reduce((a, b) => a + b, 0))
+}
+
+// console.log(busStops([[10, 0], [3, 5], [5, 8]]))
+// console.log(busStops([[3, 0], [9, 1], [4, 10], [12, 2], [6, 1], [7, 10]]))
+// console.log(busStops([[3, 0], [9, 1], [4, 8], [12, 2], [6, 1], [7, 8]]))
+
+// node "Number of People in the Bus"
+
+// Outstanding!
+// You have passed all of the tests! :)
