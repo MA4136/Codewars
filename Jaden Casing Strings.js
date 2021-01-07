8c20cfb6ea7d67a44ac165c7170efb70f75f61d0
Molotov <bigdickmasterblaster@gmail.com>
2021-01-07 14:43:17 +0300

    first shot

---
+++ Jaden Casing Strings.js
@@ -0,0 +1,22 @@
+//      Jaden Casing Strings
+//      www.codewars.com
+
+/*  Instructions
+Your task is to transform the strings so that each word in it begins with a capital letter.
+
+    Examples
+toJadenCase("do i feel lucky?") ==> "Do I Feel Lucky?"
+toJadenCase("well do ya, punk?") ==> "Well Do Ya, Punk?"
+*/
+
+function toJadenCase(string) {
+    return string.split(" ").map(el => el.replace(el[0], el[0].toUpperCase())).join(" ")
+}
+
+// console.log(toJadenCase("do i feel lucky?"))
+// console.log(toJadenCase("well do ya, punk?"))
+
+// node "Jaden Casing Strings"
+
+// Correct!
+// You have passed all of the tests! :)
