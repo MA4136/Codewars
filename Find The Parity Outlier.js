4b487155bbca6c6b89f1162ce42fc412d982f9d5
Molotov <bigdickmasterblaster@gmail.com>
2021-01-29 16:18:36 +0300

    first shot

---
+++ Find The Parity Outlier.js
@@ -0,0 +1,23 @@
+//      Find The Parity Outlier
+//      www.codewars.com
+
+/*      Instructions
+You are given an array (which will have a length of at least 3, but could be very large) containing integers.
+The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N.
+Write a method that takes the array as an argument and returns this "outlier" N.
+
+        Examples
+findOutlier([0, 1, 2])   ==> 1
+findOutlier([0,0,3,0,0]) ==> 3
+findOutlier([1,1,0,1,1]) ==> 0
+*/
+
+function findOutlier(integers) {
+    return integers.filter(el => el % 2 === 0).length === 1 ? integers.find(el => el % 2 === 0) : integers.find(el => el % 2 !== 0)
+}
+
+console.log(findOutlier([0, 1, 2]))
+console.log(findOutlier([0, 0, 3, 0, 0]))
+console.log(findOutlier([1, 1, 0, 1, 1]))
+
+// node "Find The Parity Outlier"
