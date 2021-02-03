58bc4e98456893a5d26c828d540f17ec5ec7bf65
Molotov <bigdickmasterblaster@gmail.com>
2021-02-03 16:00:22 +0300

    first shot

---
+++ The Supermarket Queue.js
@@ -0,0 +1,24 @@
+//      The Supermarket Queue
+//      www.codewars.com
+
+/*      Instructions
+full description is available here: https://www.codewars.com/kata/57b06f90e298a7b53d000a86
+
+        Examples
+queueTime([], 1) ==> 0
+queueTime([1,2,3,4], 1) ==> 10
+queueTime([2,2,3,3,4,4], 2) ==> 9
+queueTime([1,2,3,4,5], 100) ==> 5
+*/
+
+function queueTime(customers, n) {
+    const array = Array(n).fill(Number())
+    return customers.length !== 0 ? Math.max(...customers.map(el => array[array.indexOf(Math.min(...array))] += el)) : 0
+}
+
+console.log(queueTime([], 1))
+console.log(queueTime([1, 2, 3, 4], 1))
+console.log(queueTime([2, 2, 3, 3, 4, 4], 2))
+console.log(queueTime([1, 2, 3, 4, 5], 100))
+
+// node "The Supermarket Queue"
