a05367152bc81c8e554aa82c63418545f9fdf179
Molotov <bigdickmasterblaster@gmail.com>
2021-02-04 15:58:51 +0300

    first shot

---
+++ Find the unique number.js
@@ -0,0 +1,22 @@
+//      Find the unique number
+//      www.codewars.com
+
+/*      Instructions
+description is available here: https://www.codewars.com/kata/585d7d5adb20cf33cb000235
+There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+        Examples
+findUniq([ 0, 1, 0 ]) ==> 1
+findUniq([ 1, 1, 1, 2, 1, 1 ]) ==> 2
+findUniq([ 3, 10, 3, 3, 3 ]) ==> 10
+*/
+
+function findUniq(arr) {
+    return arr.find(el => arr.indexOf(el) === arr.lastIndexOf(el))
+}
+
+console.log(findUniq([0, 1, 0]))
+console.log(findUniq([1, 1, 1, 2, 1, 1]))
+console.log(findUniq([3, 10, 3, 3, 3]))
+
+// node "Find the unique number"
