6f6a8a42856aab5b925464b96aca3e26f4fa03d8
Molotov <bigdickmasterblaster@gmail.com>
2021-02-15 07:31:33 +0300

    first shot

---
+++ Valid Braces.js
@@ -0,0 +1,21 @@
+//      Valid Braces
+//      www.codewars.com
+
+/*      Instructions
+description: https://www.codewars.com/kata/5277c8a221e209d3f6000b56
+Write a function that takes a string of braces, and determines if the order of the braces is valid.
+
+        Examples
+validBraces( "()" ) ==> true
+validBraces( "[(])" ) ==> false
+*/
+
+function validBraces(braces) {
+    const regExp = new RegExp(/\{\}|\[\]|\(\)/)
+    return regExp.test(braces) ? validBraces(braces.replace(regExp, '')) : '' === braces
+}
+
+console.log(validBraces("()"))
+console.log(validBraces("[(])"))
+
+// node "Valid Braces"
