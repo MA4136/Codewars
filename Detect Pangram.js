d3a6dd64fb6078429ac0ad6765c00db4c6171012
Molotov <bigdickmasterblaster@gmail.com>
2021-02-22 16:18:42 +0300

    update

--- Detect Pangram.js
+++ Detect Pangram.js
@@ -12,10 +12,14 @@ isPangram('The quick brown fox jumps over the lazy dog.') ==> true
 isPangram('This is not a pangram.') ==> false
 */
 
-function isPangram(string = '') {
+function isPangram(string) {
     const alphabet = Array.from({length:26}, (_, idx) => String.fromCharCode(97 + idx))
     const word = string.toLowerCase().match(/[a-z]/g)
     return alphabet.every(char => word.includes(char))
+}
+
+function isPangram(string) {
+    return new Set(string.match(/[a-z]/gi)).size > 25
 }
 
 console.log(isPangram('The quick brown fox jumps over the lazy dog.'))
