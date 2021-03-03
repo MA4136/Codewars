805c5b44c42757b34bb0fbe75647a1099780478d
Molotov <bigdickmasterblaster@gmail.com>
2021-03-03 15:18:00 +0300

    update; p.2: decrypt function

--- Simple Encryption #1 - Alternating Split.js
+++ Simple Encryption #1 - Alternating Split.js
@@ -11,18 +11,41 @@ encrypt('This is a test!', 0) ==> 'This is a test!'
 encrypt('This is a test!', 1) ==> 'hsi  etTi sats!'
 encrypt('This is a test!', 3) ==> ' Tah itse sits!'
 encrypt('This is a test!', -1) ==> 'This is a test!'
+
+decrypt('This is a test!', 0) ==> 'This is a test!'
+decrypt('hsi  etTi sats!', 1) ==> 'This is a test!'
+decrypt(' Tah itse sits!', 3) ==> 'This is a test!'
+decrypt('This is a test!', -1) ==> 'This is a test!'
 */
 
 function encrypt(text, n) {
-    if (!n || n < 0) return text
-    let string = [...text].filter((el, idx) => Boolean(idx % 2)).join('') +
-                 [...text].filter((el, idx) => !Boolean(idx % 2)).join('')
+    if (!text || !n || n < 0) return text
+
+    const string = [...text].filter((_, idx) => idx % 2).join('') +
+        [...text].filter((_, idx) => !(idx % 2)).join('')
+
     return n > 1 ? encrypt(string, (n - 1)) : string
+}
+
+function decrypt(encryptedText, n) {
+    if (!encryptedText || !n || n < 0) return encryptedText
+
+    const first = encryptedText.slice(0, encryptedText.length / 2)
+    const second = encryptedText.slice(encryptedText.length / 2)
+
+    const string = [...second].map((_, idx) => second[idx].concat(first[idx] ? first[idx] : '')).join('')
+
+    return n > 1 ? decrypt(string, (n - 1)) : string
 }
 
 console.log(encrypt('This is a test!', 0))
 console.log(encrypt('This is a test!', 1))
 console.log(encrypt('This is a test!', 3))
 console.log(encrypt('This is a test!', -1))
+
+console.log(decrypt('This is a test!', 0))
+console.log(decrypt('hsi  etTi sats!', 1))
+console.log(decrypt(' Tah itse sits!', 3))
+console.log(decrypt('This is a test!', -1))
 
 // node "Simple Encryption #1 - Alternating Split"
