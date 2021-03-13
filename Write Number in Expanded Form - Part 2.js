f78d53da82fe57cad8f9c8604a609923a6f13c66
Molotov <bigdickmasterblaster@gmail.com>
2021-03-13 21:34:27 +0300

    update

--- Write Number in Expanded Form - Part 2.js
+++ Write Number in Expanded Form - Part 2.js
@@ -1,5 +1,4 @@
 //      Write Number in Expanded Form - Part 2
-//      www.codewars.com
 
 /*      Instructions
 description: https://www.codewars.com/kata/write-number-in-expanded-form-part-2
@@ -8,18 +7,18 @@ You will be given a number and you will need to return it as a string in Expande
         Examples
 expandedForm(1.24) ==> '1 + 2/10 + 4/100'
 expandedForm(7.304) ==> '7 + 3/10 + 4/1000'
-expandedForm(88.353824), ' ==> 80 + 8 + 3/10 + 5/100 + 3/1000 + 8/10000 + 2/100000 + 4/1000000'
+expandedForm(88.353824) ==> '80 + 8 + 3/10 + 5/100 + 3/1000 + 8/10000 + 2/100000 + 4/1000000'
 */
 
 function expandedForm(num) {
-    const first = [...String(Math.trunc(num))]
+    const [firstNum, secondNum] = num.toString().split('.')
+
+    const first = [...String(firstNum)]
         .map((el, idx, array) => el + '0'.repeat((array.length - idx) - 1))
         .filter(el => Number(el) !== 0)
         .join(' + ')
 
-    const trunc = String(num).match(/(?<=\.)[0-9]+/g) // any numbers after the dot
-
-    const second = [...String(trunc)].map((el, idx) => el + `/${10 ** (idx + 1)}`)
+    const second = [...String(secondNum)].map((el, idx) => el + `/${10 ** (idx + 1)}`)
         .filter(el => Number(el[0]) !== 0)
         .join(' + ')
 
--- Write Number in Expanded Form.js
+++ Write Number in Expanded Form.js
@@ -1,5 +1,4 @@
 //      Write Number in Expanded Form
-//      www.codewars.com
 
 /*      Instructions
 description: https://www.codewars.com/kata/5842df8ccbd22792a4000245
@@ -7,14 +6,14 @@ You will be given a number and you will need to return it as a string in Expande
 
         Examples
 expandedForm(12) ==> '10 + 2'
-expandedForm(42) ==> 40 + 2'
+expandedForm(42) ==> '40 + 2'
 expandedForm(70304) ==> '70000 + 300 + 4'
 */
 
 function expandedForm(num) {
     return [...String(num)]
         .map((el, idx, array) => el + '0'.repeat((array.length - idx) - 1))
-        .filter(el => Number(el) ? el : '')
+        .filter(el => Number(el) !== 0)
         .join(' + ')
 }
 
