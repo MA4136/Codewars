adb88d82b01e473592ca03fcbecf5f081e4d9e49
Molotov <bigdickmasterblaster@gmail.com>
2021-03-16 18:04:53 +0300

    first shot

---
+++ Mexican Wave.js
@@ -0,0 +1,22 @@
+//      Mexican Wave
+
+/*      Instructions
+description: https://www.codewars.com/kata/58f5c63f1e26ecda7e000029
+
+        Examples
+wave("") ==> []
+wave(" gap ") ==> [" Gap ", " gAp ", " gaP "]
+wave("hello") ==> ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+wave("codewars") ==> ["Codewars", "cOdewars", "coDewars", "codEwars", "codeWars", "codewArs", "codewaRs", "codewarS"]
+*/
+
+function wave(str) {
+    return str ? Array.from(str, (el, idx) => el !== ' ' && str.slice(0, idx) + el.toUpperCase() + str.slice(++idx)).filter(Boolean) : []
+}
+
+console.log(wave(''))
+console.log(wave(' gap '))
+console.log(wave('hello'))
+console.log(wave('codewars'))
+
+// node "Mexican Wave"
