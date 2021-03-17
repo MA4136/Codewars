f758381f005a1b0ccd1dacfe1a68d916b0e35010
Molotov <bigdickmasterblaster@gmail.com>
2021-03-17 16:53:00 +0300

    first shot(not finished)

---
+++ Roman Numerals Decoder.js
@@ -0,0 +1,29 @@
+//      Roman Numerals Decoder
+
+/*      Instructions
+description: https://www.codewars.com/kata/51b6249c4612257ac0000005
+Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer.
+
+        Examples
+solution('I') ==> 1
+solution('IV') ==> 4
+solution('XXI') ==> 21
+solution('CDXLIV') ==> 444
+solution('MDCLXVI') ==> 1666
+*/
+
+function solution(roman) {
+    const numerals = {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000}
+    let result = 0
+    return [...roman].reduce((_, curr, idx, array) => {
+        return (numerals[curr] < numerals[array[idx + 1]]) ? (result -= numerals[curr]) : (result += numerals[curr])
+    }, 0)
+}
+
+console.log(solution('I'))
+console.log(solution('IV'))
+console.log(solution('XXI'))
+console.log(solution('CDXLIV'))
+console.log(solution('MDCLXVI'))
+
+// node "Roman Numerals Decoder"
