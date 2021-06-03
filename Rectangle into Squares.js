730249a067e03a4820bc73b2f2fe50370ee49145
Molotov <bigdickmasterblaster@gmail.com>
2021-06-03 20:52:03 +0300

    update

--- Rectangle into Squares.js
+++ Rectangle into Squares.js
@@ -32,6 +32,12 @@ function sqInRect(lng, wdth, flag = true) {
     return [min, ...sqInRect(max - min, min, false)]
 }
 
+// #3
+function sqInRect(lng, wdth, recursive) {
+    recursive = recursive === undefined ? false : recursive
+    return lng === wdth && recursive === false ? null : lng === wdth ? [lng] : [Math.min(lng, wdth)].concat(sqInRect(Math.min(lng, wdth), Math.max(lng, wdth) - Math.min(lng, wdth), true))
+}
+
 console.log(sqInRect(5, 5))
 console.log(sqInRect(5, 3))
 console.log(sqInRect(3, 5))
