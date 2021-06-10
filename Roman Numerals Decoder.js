b03850c58424a1377f77d053073341f1c3318843
Molotov <bigdickmasterblaster@gmail.com>
2021-06-10 22:10:37 +0300

    update

--- Roman Numerals Decoder.js
+++ Roman Numerals Decoder.js
@@ -14,7 +14,9 @@ solution('MDCLXVI') ==> 1666
 
 // #1
 function solution(roman) {
-    const numerals = {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000}
+    const numerals = {
+        I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000
+    }
     let result = 0
     return [...roman].reduce((_, curr, idx, array) => {
         return (numerals[curr] < numerals[array[idx + 1]]) ? (result -= numerals[curr]) : (result += numerals[curr])
@@ -26,7 +28,15 @@ function solution(roman) {
     const numerals = {
         M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1
     }
-    return roman.match(/CM|CD|XC|XL|IX|IV|\w/g).reduce((accum, roman) => accum + numerals[roman], 0)
+    return roman.match(/CM|CD|XC|XL|IX|IV|\w/g).reduce((acc, roman) => acc + numerals[roman], 0)
+}
+
+// #3
+function solution(roman) {
+    const numerals = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+    }
+    return [...roman].map(el => numerals[el]).reduce((acc, curr) => acc < curr ? curr - acc : acc + curr)
 }
 
 console.log(solution('I'))
